b5b3a4436e6ebc9550df4b5d870dad8b9f506c2e
BJR-Developer <freelancerbjr@gmail.com>
2022-04-27 22:03:46 +0600

    test convert to Test

--- pages/test.js
+++ pages/test.js
@@ -1,29 +1,29 @@
 import React from 'react'
 import SimpleReactFileUpload from '../components/mainbar/TestUp'
 
- const test = () => {
-   const [ selectFile , setSelect ] = React.useState([]);
-   console.log(selectFile);
+const Test = () => {
+  const [selectFile, setSelect] = React.useState([]);
+  console.log(selectFile);
 
-   const submitData = async() =>{
-     const formData = new FormData();
-     formData.append("file" , selectFile);
+  const submitData = async () => {
+    const formData = new FormData();
+    formData.append("file", selectFile);
 
-     formData.append('upload_preset', 'fb-upload-management');
+    formData.append('upload_preset', 'fb-upload-management');
 
-     const dataSend = await fetch("https://api.cloudinary.com/v1_1/dxtnn1awt/image/upload" , 
+    const dataSend = await fetch("https://api.cloudinary.com/v1_1/dxtnn1awt/image/upload",
       {
-        method:"post",
-        body:formData,
+        method: "post",
+        body: formData,
       }
-     ).then(res=>res.json())
-      console.log("dataSend" , dataSend);
-   }
+    ).then(res => res.json())
+    console.log("dataSend", dataSend);
+  }
   return (
     <div className=' min-h-screen flex items-center justify-center'>
-        <input type="file" onChange={(e)=>setSelect(e.target.files[0])}/>
-        <button onClick={submitData}>Submit Data</button>
+      <input type="file" onChange={(e) => setSelect(e.target.files[0])} />
+      <button onClick={submitData}>Submit Data</button>
     </div>
   )
 }
-export default test
+export default Test
